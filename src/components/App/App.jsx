80f0406a8d4743a0cfd1e3edcc7fbe373ba6c80d
Chris Cantoni <chris.cantoni@gmail.com>
2023-11-09 11:48:59 -0600

    Local App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,41 +2,20 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer.jsx';
-import '../Footer/Footer.css'
+import Cart from '../Cart/Cart';
 import SelectPizza from '../SelectPizza/SelectPizza';
-
-
 
 function App() {
 
-  // const [pizzaList, setPizzaList] = useState([]);
-
-  // const getPizzaList = () => {
-  //   axios.get('/api/pizza').then((response) => {
-  //       console.log(response.data);
-  //       setPizzaList(response.data)
-  //   }).catch((error) => {
-  //       console.log('GET /pizza error', error);
-  //       alert('Something went wrong fetching pizzas');
-  //   })
-  // }
-
-  // useEffect(()=> {
-  //   getPizzaList()
-  // }, []);
-
   return (
     <div className='App'>
-      <Header/> 
-      <br></br>
+      <header className='App-header'>
+        <h1 className='App-title'>Prime Pizza</h1>
+      </header>
+    
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
       <SelectPizza/>
- 
- 
-      <Footer />
     </div>
   );
 }
