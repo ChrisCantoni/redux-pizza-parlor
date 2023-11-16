2433d97d99680d2e9e88f7958ecdba05724f323e
Chris Cantoni <chris.cantoni@gmail.com>
2023-11-16 13:04:13 -0600

    Added Next button on Select Pizza page.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,22 @@
 import ShoppingCart from '@mui/icons-material/ShoppingCart';
+import {Link, useHistory} from 'react-router-dom';
+
 import Button from '@mui/material/Button';
 import { useSelector } from 'react-redux';
 
+
 const Cart = () => {
 
+    const history = useHistory();
     const total = useSelector(store => store.total);
+
+    const handleClick = () => {
+        history.push('/info');
+    }
 
     return (
         <Button id="Cart" variant="contained" size="xl" endIcon={<ShoppingCart id="cart-icon"/>}>${total}</Button>
+        
     )
 }
 
